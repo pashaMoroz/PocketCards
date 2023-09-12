eae3a2a34262331264a54e997307a49190a2785a
Moroz Pavlo <pavlo@sidekickinteractive.com>
2023-09-12 09:11:07 -0400

    Initial Commit

---
+++ PocketCards/PocketCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCardsApp.swift
+//  PocketCards
+//
+//  Created by Moroz Pavlo on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct PocketCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
